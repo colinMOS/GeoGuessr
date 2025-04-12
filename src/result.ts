4f4bc2211c925129edffb895e11f64d026fa7872
cOliN24 <col.pfeilmaier.24@lehre.mosbach.dhbw.de>
2025-04-12 23:17:56 +0200

    fix(result): remove victory meme, multiple small fixes

--- src/result.ts
+++ src/result.ts
@@ -1,59 +1,38 @@
-import { checkDistance } from './calculation';
-import { roundPanel } from './guessMap';
-import L, { LatLng } from 'leaflet';
-const resultPanel = document.getElementById("resultPanel")
+import { roundPanel } from './guess';
+import { guessMap, nextButton } from './dom-utils';
+import L from 'leaflet';
+const resultPanel = document.getElementById("ResultPanel")
+export let roundMap: L.Map | null = null;
 
-export function setRoundMap(coords1: { lat: number, lng: number }, coords2: L.LatLng): void {
-    var roundMap = L.map('RoundMap').setView([coords1.lat, coords1.lng], 15);
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(roundMap);
-
+export function setRoundMap(
+    coords1: { lat: number, lng: number }, 
+    coords2: L.LatLng
+): void {
+    // no map -> initialize, else: update map
+    if(!roundMap) {
+        roundMap = L.map('RoundMap').setView([coords1.lat, coords1.lng], 15);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(roundMap);
+    } else {
+        roundMap.setView([coords1.lat, coords1.lng], 15);
+    }
+    
     // Popup jsonCoords
-    var jsonPopup = L.marker([coords1.lat, coords1.lng],
-        {   title: 'Ziel'
-        }
-        ).addTo(roundMap);
+    L.marker([coords1.lat, coords1.lng], { title: 'Ziel' }).addTo(roundMap);
     // Popup guessedCoords
-    var guessPopup = L.marker([coords2.lat, coords2.lng],
-        {   title: 'Dein Guess'
-        }
-    ).addTo(roundMap);
+    L.marker([coords2.lat, coords2.lng], { title: 'Dein Guess' }).addTo(roundMap);
+}
 
-    checkDistance(coords1, coords2);
-
+export function changeNextButton() {
+    if (nextButton) {
+        nextButton.innerHTML = "Ergebnisse ansehen";
+        nextButton.style.background = "#FFA43D";
+        nextButton.style.color = "#1A1A2E";
+    }
 }
 
 export function showResults() {
-    const resultButton: HTMLButtonElement | null = document.querySelector("#nextRound");
-    const memeIframe = document.querySelector("#Meme") as HTMLIFrameElement | null;;
-    if (resultButton) {
-       resultButton.innerHTML = "Ergebnisse ansehen"
-    }
-
-    resultButton?.addEventListener("click", function() {
-        // hide roundPanel
-        roundPanel?.classList.remove("show");
-        // show resultPanel
-        resultPanel?.classList.remove("show");
-        // show vitory meme
-        fetchMemeUrl().then((url) => {
-            if (url && memeIframe) {
-                memeIframe.src = url;
-            }
-          });
-    })
-}
-
-async function fetchMemeUrl(): Promise<string | null> {
-    try {
-      const response = await fetch('https://meme-api.com/gimme');  
-      const data = await response.json();
-
-      return data.url as string;
-    } catch (error) {
-      console.error('Failed to fetch meme URL:', error);
-      return null;
-    }
+    // hide roundPanel
+    roundPanel?.classList.remove("show");
+    // show resultPanel
+    resultPanel?.classList.add("show");
 }
