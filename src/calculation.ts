0b702e360c41ce1cb30a3a5215d0e097db9c3340
cOliN24 <col.pfeilmaier.24@lehre.mosbach.dhbw.de>
2025-04-12 23:16:00 +0200

    fix(calculation): fix totalScore calculation

--- src/calculation.ts
+++ src/calculation.ts
@@ -1,6 +1,7 @@
-export function checkDistance(coords1: { lat: number, lng: number }, coords2: L.LatLng): string {   
-    const R = 6371; // Earth's radius in kilometers
+let totalScore = 0;
+const R = 6371; // Earth's radius in km
 
+export function checkDistance(coords1: { lat: number, lng: number }, coords2: L.LatLng): string {   
     // Convert degrees to radians
     const lat1 = degreesToRadians(coords1.lat);
     const lon1 = degreesToRadians(coords1.lng);
@@ -16,22 +17,19 @@ export function checkDistance(coords1: { lat: number, lng: number }, coords2: L.
         Math.sin(diffLon / 2) * Math.sin(diffLon / 2);
 
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in kilometers
-    const distRound = distance.toFixed(2);
-    showDistance(distRound)
+    const distance = R * c; // Distance in km
+    const distanceRound = distance.toFixed(2);
+    showDistance(distanceRound)
     calcPoints(distance);
     
-
-    return distRound;
+    return distanceRound;
 }
 
 function showDistance(distance: string) {
-    const distText = document.querySelector(".distance");
-    if (distText) {
-        distText.innerHTML = distance;
+    const distanceText = document.querySelector(".distance");
+    if (distanceText) {
+        distanceText.innerHTML = distance;
     }
-
-    console.log(`Distance between guess and actual location: ${distance} km`);
 }
 
 // Helper function to convert degrees to radians
@@ -39,30 +37,37 @@ function degreesToRadians(degrees: number): number {
     return degrees * (Math.PI / 180);
 }
 
-export function calcPoints(distance: number): number {
+export function calcPoints(distance: number): void {
     let points: number = 0;
-    let Scores = document.querySelectorAll(".score")
+    let pointText = document.querySelector(".points");
+    let ScoreTexts = document.querySelectorAll(".score");
 
     if (distance > 10000) {
-        points = 50;
+        points += 50;
     } else if (distance > 5000) {
-        points = 200;
+        points += 200;
     } else if (distance > 3000) {
-        points = 300;
+        points += 300;
     } else if (distance > 2000) {
-        points = 400;
+        points += 400;
     } else if (distance >= 1000) {
-        points = 500;
+        points += 500;
     } else if (distance >= 100) {
-        points = 900;
+        points += 900;
     } else if (distance >= 50) {
-        points = 1000;
+        points += 1000;
     }
-
-    if (Scores) {
-        Scores.forEach(score => {
-            score.innerHTML = points.toString();
+    totalScore += points;
+    
+    if (ScoreTexts)
+        ScoreTexts.forEach(scoreText => {
+            scoreText.innerHTML = totalScore.toString();
         })
-    }
-    return points;
+            
+    if (pointText) 
+        pointText.innerHTML = points.toString();
+}
+
+export function getTotalScore(): number {
+    return totalScore;
 }
